55cd3b27133b8260667bca7c002d4ace773b0bcb
Polar <tpwns73@gmail.com>
2023-09-21 17:45:14 +0900

    [fix] fix variables

--- dags/test.py
+++ dags/test.py
@@ -35,9 +35,11 @@ def task_test_query():
         )
         
         for shape_label_row in shape_label_rows:
-            print(shape_label_row)
+            annotation = hook.get_records("SELECT x.name, y.points FROM public.engine_label x JOIN public.engine_labeledshape y ON x.id = y.label_id WHERE y.label_id = %s", parameters=(shape_label_row[0]))
+            print(annotation)
         for track_label_row in track_label_rows:
-            print(track_label_row)
+            annotation = hook.get_records("SELECT x.name FROM public.engine_label x JOIN public.engine_labeledtrack y ON x.id = y.label_id WHERE y.label_id = %s", parameters=(track_label_row[0]))
+            print(annotation)
         
 task_1 = PythonOperator(
     task_id = "run_query_with_python",
