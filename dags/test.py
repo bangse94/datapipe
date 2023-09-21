ba013cb25ca50871deb98e1872aa9010e0967cf2
Polar <tpwns73@gmail.com>
2023-09-21 11:38:49 +0900

    postgres get user

--- dags/test.py
+++ dags/test.py
@@ -5,18 +5,27 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.models.connection import Connection
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-ENV_ID = os.environ.get("SYSTEM_ENV_ID")
+con = Connection(
+    conn_id = 'cvat_postgres',
+    conn_type = 'postgres',
+    login = 'root',
+    password = ''
+)
 
-with DAG(
-    dag_id = "test-dag",
-    start_date=datetime(2023, 1, 1),
-    schedule='@once',
-    catchup=False,
-) as dag:
-    get_update_date = PostgresOperator(
-        task_id='get_update_date',
-        sql = "SELECT * FROM engine_task WHERE update_date BETWEEN %(begin_date)s AND %(end_date)s",
-        parameters = {"begin_date": "2023-09-19", "end_date": "2023-09-20"},
-        hook_params = {"options": "-c statement_timeout=3000ms"},
-    )
+def task_test_query():
+    hook = PostgresHook(postgres_conn_id='cvat_postgres')
+    
+    rows = hook.get_records("SELECT * FROM users")
+    
+    for row in rows:
+        print(row)
+
+task_1 = PythonOperator(
+    task_id = "run_query_with_python",
+    python_callable = task_test_query,
+    dag = dag
+)
