ebaea03f2fddc53d3884c2e388a4baead07ce845
Sejun Park <tpwns73@gmail.com>
2023-09-22 16:46:11 +0900

    [feat] object name, points

--- dags/test.py
+++ dags/test.py
@@ -25,20 +25,23 @@ dag = DAG(
 def task_test_query():
     hook = PostgresHook(postgres_conn_id='cvat_postgres')
     
-    job_ids = hook.get_records("SELECT x.id FROM public.engine_job x WHERE x.stage = 'acceptance' and x.updated_date < current_date and x.updated_date >= current_date-1")
+    #job_ids = hook.get_records("SELECT x.id FROM public.engine_job x WHERE x.stage = 'acceptance' and x.updated_date < current_date and x.updated_date >= current_date-1")
+    job_ids = hook.get_records("SELECT x.id FROM public.engine_job x WHERE x.stage = 'acceptance' and x.updated_date < current_date+1")
     
     for job_id in job_ids:
-        shape_label_rows = hook.get_records("SELECT x.label_id, x.points FROM public.engine_labeledshape x WHERE x.job_id = %s", parameters=(job_id))
+        shape_label_rows = hook.get_records("SELECT x.label_id FROM public.engine_labeledshape x WHERE x.job_id = %s", parameters=(job_id))
         #track_label_rows = hook.get_records("SELECT id, label_id points FROM public.engine_trackedshape WHERE job_id = %s", parameters=(job_id))
         track_label_rows = hook.get_records(
-            "SELECT a.label_id, b.points FROM public.engine_labeledtrack a JOIN public.engine_trackedshape b ON a.id = b.track_id WHERE a.job_id = %s", parameters=(job_id)
+            "SELECT a.label_id FROM public.engine_labeledtrack a JOIN public.engine_trackedshape b ON a.id = b.track_id WHERE a.job_id = %s", parameters=(job_id)
         )
+        print("shape",shape_label_rows)
+        print("track",track_label_rows)
         
         for shape_label_row in shape_label_rows:
-            annotation = hook.get_records("SELECT x.name, y.points FROM public.engine_label x JOIN public.engine_labeledshape y ON x.id = y.label_id WHERE y.label_id = %s", parameters=([shape_label_row[0]]))
+            annotation = hook.get_records("SELECT x.name, y.points FROM public.engine_label x, public.engine_labeledshape y WHERE x.id = y.label_id AND y.label_id = %s", parameters=([shape_label_row[0]]))
             print(annotation)
         for track_label_row in track_label_rows:
-            annotation = hook.get_records("SELECT x.name FROM public.engine_label x JOIN public.engine_labeledtrack y ON x.id = y.label_id WHERE y.label_id = %s", parameters=([track_label_row[0]]))
+            annotation = hook.get_records("SELECT x.name, z.points FROM public.engine_label x, public.engine_labeledtrack y, public.engine_trackedshape z WHERE x.id = y.label_id and z.track_id = y.id and y.label_id = %s", parameters=([track_label_row[0]]))
             print(annotation)
         
 task_1 = PythonOperator(
