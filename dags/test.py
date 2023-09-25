f3b60154beaec68c27d9b45516c4761031a40488
Polar <tpwns73@gmail.com>
2023-09-25 13:47:39 +0900

    [feat] file name test

--- dags/test.py
+++ dags/test.py
@@ -39,11 +39,24 @@ def task_test_query():
         
         for shape_label_row in shape_label_rows:
             annotation = hook.get_records("SELECT x.name, y.points FROM public.engine_label x, public.engine_labeledshape y WHERE x.id = y.label_id AND y.label_id = %s", parameters=([shape_label_row[0]]))
-            print(annotation)
+            frame_num = hook.get_records("SELECT x.frame FROM public.engine_labeledshape x WHERE x.label_id = %s", parameters=([shape_label_row[0]]))
+            segment_id = hook.get_records("SELECT x.segment_id FROM public.engine_job x WHERE x.id = %s", parameters=([job_id]))
+            task_id = hook.get_records("SELECT x.task_id FROM public.engine_segment x WHERE x.id = %s", parameters=([segment_id]))
+            data_id = hook.get_records("SELECT x.data_id FROM public.engine_task x WHERE x.id = %s", parameters=([task_id]))
+            file_name = hook.get_records("SELECT x.path FROM public.engine_image x WHERE x.data_id = %s and x.frame = %s", parameters=([data_id], [frame_num]))
+            print(file_name, annotation)
         for track_label_row in track_label_rows:
             annotation = hook.get_records("SELECT x.name, z.points FROM public.engine_label x, public.engine_labeledtrack y, public.engine_trackedshape z WHERE x.id = y.label_id and z.track_id = y.id and y.label_id = %s", parameters=([track_label_row[0]]))
-            print(annotation)
+            frame_num = hook.get_records("SELECT x.frame FROM public.engine_labeledtrack.frame x WHERE x.label_id = %s", parameters([track_label_row[0]]))
+            segment_id = hook.get_records("SELECT x.segment_id FROM public.engine_job x WHERE x.id = %s", parameters=([job_id]))
+            task_id = hook.get_records("SELECT x.task_id FROM public.engine_segment x WHERE x.id = %s", parameters=([segment_id]))
+            data_id = hook.get_records("SELECT x.data_id FROM public.engine_task x WHERE x.id = %s", parameters=([task_id]))
+            file_name = hook.get_records("SELECT x.path FROM public.engine_image x WHERE x.data_id = %s and x.frame = %s", parameters=([data_id], [frame_num]))
+            print(file_name, annotation)
         
+    # engine_labledshape.frame == engine_image.frame, engine_labeledtrack.frame == engine_image.frame
+    # engine_job.segment_id -> engine_segment.task_id -> engine_task.data_id = engine_image.data_id -> engine_image.name
+    
 task_1 = PythonOperator(
     task_id = "run_query_with_python",
     python_callable = task_test_query,
