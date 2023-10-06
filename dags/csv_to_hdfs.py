98ff6a14245c162e1dfbf7aad1317ef02321cbdb
Polar <tpwns73@gmail.com>
2023-10-06 17:13:35 +0900

    [feat] hive and spark

--- dags/csv_to_hdfs.py
+++ dags/csv_to_hdfs.py
@@ -1,5 +1,4 @@
 import os
-from os.path import expanduser, join, abspath
 import io
 
 import pandas as pd
@@ -14,40 +13,29 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.providers.apache.hive.hooks.hive import *
-from airflow.providers.apache.spark import SparkOperator
+from airlfow.providers.apache.hive import HiveOperator
 
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_csv
-
-con = Connection(
+cvat_con = Connection(
     conn_id = 'cvat_postgres',
     conn_type = 'postgres',
     login = 'root',
     password = ''
 )
 
-spark = SparkSession \
-    .builder \
-        .appName("cvat-spark") \
-            .config("spark.sql.warehouse.dir", "/home/sjpark/warehouse") \
-                .enableHiveSupport() \
-                    .getOrCreate()
-                    
-df = spark.read.csv(f"/home/sjpark/test{datetime.now().strftime('%Y%m%d')}.csv", header=True)
-
-labels = df.select('file_name', 'class', 'points') \
-    .dropDuplicates(['file_name', 'class', 'points'])        
-
-labels.write.mode("append").insertInto("labels")
+db_con = Connection(
+    conn_id = 'label_db',
+    conn_type = 'postgres',
+    login = 'admin',
+    password = 'unibase0910'
+)
 
 dag = DAG(
-    dag_id = "cvat-spark-dag",
+    dag_id = "cvat-hdfs-dag",
     start_date = days_ago(2),
     schedule_interval = "@once"
 )
 
-def get_annotation_query(**context):
+def cvat_to_csv():
     hook = PostgresHook(postgres_conn_id='cvat_postgres')
     
     #job_ids = hook.get_records("SELECT x.id FROM public.engine_job x WHERE x.stage = 'acceptance' and x.updated_date < current_date and x.updated_date >= current_date-1")
@@ -67,28 +55,13 @@ def get_annotation_query(**context):
                         AND a.segment_id = b.id AND b.task_id = c.id AND x.data_id = c.data_id AND z.frame = x.frame", parameters=(job_id, job_id))
     
     df = pd.DataFrame(data=shape_label_rows+track_label_rows, columns=['file_name', 'class', 'points'])
-    try:
-        origin = pd.read_csv(f"/home/sjpark/test{datetime.now().strftime('%Y%m%d')}.csv", index_col=0)
-        df = pd.concat([origin, df], ignore_index=True)
-        df.drop_duplicates(subset=None, keep='first', inplace=True)
-        df.reset_index()
-    except:
-        pass    
-    df.to_csv(f"/home/sjpark/test{datetime.now().strftime('%Y%m%d')}.csv",header=True, mode='w')
+    df.drop_duplicates()
+    df.to_csv(f"/home/sjpark/test{datetime.now().strftime('%Y%m%d')}.csv",header=False, mode='a')
     
-def create_hdfs_table():
-    hm = HiveServer2Hook(hiveserver2_conn_id = "hiveserver2_warehouse")
-    hql = """
-        CREATE EXTERNAL TABLE IF NOT EXIST labes(
-            file_name STRING,
-            class STRING,
-            points STRING
-        )
-        ROW FORMAT DELIMITED
-        FIELDS TERMINATED BY ','
-        STORED AS TEXTFILE
-    """
-
+def csv_to_sql():
+    hook = PostgresHook(postgres_conn_id='label_db')
+    
+    
             
 def start_func():
     print('DAG start')
@@ -104,27 +77,8 @@ start_task = PythonOperator(
 
 get_annotation = PythonOperator(
     task_id = "get_labels",
-    python_callable = get_annotation_query,
+    python_callable = cvat_to_csv,
     dag = dag
-)
-
-save_csv_hdfs = BashOperator(
-    task_id = "save_hdfs",
-    bash_command = "hdfs dfs -put /home/sjpark/test{{ execution_date.strftime('%Y%m%d') }}.csv /home/sjpark/warehouse",
-    dag = dag
-)
-
-crate_hive_table = PythonOperator(
-    task_id = "create_hive_table",
-    python_callable = create_hdfs_table,
-    dag = dag
-)
-
-spark_processing = SparkSubmitOperator(
-    task_id = "spark_processing",
-    application = "/home/sjpark/airflow/dags/spark_processing.py",
-    conn_id = "spark_master",
-    verbose = False,
 )
 
 end_task = PythonOperator(
