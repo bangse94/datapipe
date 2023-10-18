a6e9901de9258dcfb5268823f820c17e64db3023
Polar <tpwns73@gmail.com>
2023-10-18 16:50:40 +0900

    [FIX] get validated annotation

--- dags/dag_Spark.py
+++ dags/dag_Spark.py
@@ -29,14 +29,20 @@ con = Connection(
 dag = DAG(
     dag_id = "cvat-spark-dag",
     start_date = days_ago(2),
-    schedule_interval = "@once"
+    schedule_interval = "@daily"
 )
 
 def get_annotation_query(**context):
     hook = PostgresHook(postgres_conn_id='cvat_postgres')
     
     #job_ids = hook.get_records("SELECT x.id FROM public.engine_job x WHERE x.stage = 'acceptance' and x.updated_date < current_date and x.updated_date >= current_date-1")
-    job_ids = hook.get_records("SELECT x.id FROM public.engine_job x WHERE x.stage = 'acceptance' and x.updated_date < current_date+1")
+    job_ids = hook.get_records("""
+                            SELECT x.id FROM public.engine_job x, public.engine_segment y, public.engine_task z, public.engine_project w
+                            where z.project_id = 32 and w.id = 32
+                            and y.task_id = z.id
+                            and x.segment_id = y.id
+                            and x.stage = 'acceptance'
+                            """)
     res = []
     for job_id in job_ids:
         # file_name, class_name, bbox
@@ -53,18 +59,18 @@ def get_annotation_query(**context):
     
     df = pd.DataFrame(data=shape_label_rows+track_label_rows, columns=['file_name', 'class', 'points'])
     try:
-        origin = pd.read_csv(f"/home/sjpark/test{datetime.now().strftime('%Y%m%d')}.csv", index_col=0)
+        origin = pd.read_csv(f"/home/sjpark/validated_{datetime.now().strftime('%Y%m%d')}.csv", index_col=0)
         df = pd.concat([origin, df], ignore_index=True)
         df.drop_duplicates(subset=None, keep='first', inplace=True)
         df.reset_index()
     except:
         pass    
-    df.to_csv(f"/home/sjpark/test{datetime.now().strftime('%Y%m%d')}.csv",header=True, mode='w')
+    df.to_csv(f"/home/sjpark/validated_{datetime.now().strftime('%Y%m%d')}.csv",header=True, mode='w')
     
 def create_hdfs_table():
     hm = HiveServer2Hook(hiveserver2_conn_id = "hiveserver2_warehouse")
     hql = """
-        CREATE EXTERNAL TABLE IF NOT EXIST labes(
+        CREATE EXTERNAL TABLE IF NOT EXIST labels(
             file_name STRING,
             class STRING,
             points STRING
@@ -91,10 +97,10 @@ get_annotation = PythonOperator(
     python_callable = get_annotation_query,
     dag = dag
 )
-today = datetime.now().strftime('%Y%m%d')
+
 save_csv_hdfs = BashOperator(
     task_id = "save_hdfs",
-    bash_command = "/home/sjpark/hadoop-3.2.4/bin/hdfs dfs -put /home/sjpark/test{{ execution_date.strftime('%Y%m%d') }}.csv /home/sjpark/warehouse",
+    bash_command = "/home/sjpark/hadoop-3.2.4/bin/hdfs dfs -put /home/sjpark/validated_{datetime.now().strftime('%Y%m%d')}.csv /home/sjpark/warehouse",
     dag = dag
 )
 
@@ -104,32 +110,10 @@ crate_hive_table = PythonOperator(
     dag = dag
 )
 
-spark = SparkSession \
-    .builder \
-        .appName("cvat-spark") \
-            .config("spark.sql.warehouse.dir", "/home/sjpark/warehouse") \
-                .enableHiveSupport() \
-                    .getOrCreate()
-                    
-#df = spark.read.csv(f"/home/sjpark/test{datetime.now().strftime('%Y%m%d')}.csv", header=True)
-#
-#labels = df.select('file_name', 'class', 'points') \
-#    .dropDuplicates(['file_name', 'class', 'points'])        
-#
-#labels.write.mode("append").insertInto("labels")
-#
-#spark_processing = SparkSubmitOperator(
-#    task_id = "spark_processing",
-#    application = "/home/sjpark/airflow/dags/spark_processing.py",
-#    conn_id = "spark_master",
-#    verbose = False,
-#)
-
 end_task = PythonOperator(
     task_id = "end_task",
     python_callable = end_func,
     dag = dag
 )
 
-#start_task >> get_annotation >> save_csv_hdfs >> crate_hive_table >> spark_processing >> end_task
-start_task >> get_annotation >> save_csv_hdfs >> crate_hive_table >> end_task
+start_task >> get_annotation >> remove_csv_hdfs >> save_csv_hdfs >> create_hive_table >> end_task
